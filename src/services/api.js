c88727f9fb4985c8a4b6f0b2d19bae3134a5f005
Shivam Pandey <Atharvp937@gmail.com>
2025-08-31 14:02:23 +0530

    new

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://studynotion-backend-yty3.onrender.com"
+const BASE_URL = "https://studynotion-backend-yty3.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
